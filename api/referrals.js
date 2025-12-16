36ad87cb0ad2d9d17034f2e243740abdcd4444aa
MPQuestoria <drenip001@gmail.com>
2025-12-16 07:16:38 +0300

    Update referrals.js

--- api/referrals.js
+++ api/referrals.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: "No telegramId" });
   }
 
-  const { data = [], error } = await supabase
+  const { data, error } = await supabase
     .from("players")
     .select("id, username")
     .eq("referrer_id", telegramId);
